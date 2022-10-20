c765d9d1c48589dcd1b5cc73858bcc1c04427057
Jose Carlos Valderrama <115050654+jcvb2391@users.noreply.github.com>
2022-10-21 01:57:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+showNotes();
+
+// si usas añadir una nota se guardara en el almacenamiento local
+
+let addBtn = document.getElementById("addBtn");
+addBtn.addEventListener("click", function(e) {
+    let addTxt = document.getElementById("addTxt");
+    let notes = localStorage.getItem("notes");
+    if (notes == null){
+        notesObj = {};
+    }else {
+        notesObj = JSON.parse(notes);
+    }
+    notesObj.push(addTxt.value);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    addTxt.value ="";
+    showNotes();
+});
+
+// funciones para mostrar elementos desde el almacenamiento local
+
+function showNotes(){
+    let notes = localStorage.getItem("notes");
+    if (notes == null){
+        notesObj = {};
+    }else {
+        notesObj = JSON.parse("notes");
+    }
+}
+
+let html = "";
+notesObj.forEach(function (element, index ){
+    html += 
+    <div class = "noteCard my-2 mx-2 card" style="width 18 rem">
+        <div>
+            <h5 class = "card-title">Nota ${index +1}</h5>
+            <p class = "card-text">${element}</p>
+            <button id = "${index}" onclick="deleteNote(this.id)" class="btn btn-danger">Borrar Nota</button>
+        </div>
+    </div>
+});
+
+let notesElm = document.getElementById("notes");
+
+if (notesObj.length !=0) {
+    notesElm.innerHTML= html;
+}else {
+    notesElm.innerHTML = "Nada que mostrar! Crea tu primera nota!"; 
+}
+
+// funciones para borrar una nota 
+
+function deleteNote(index) {
+    let notes = localStorage.getItem("notes");
+    if (notes == null){
+        notesObj = {};
+    }else {
+        notesObj = JSON.parse("notes");
+    }
+    notesObj.splice(index, 1);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    showNotes();
+}
